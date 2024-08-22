bd7a169fc0dea5c650e6ebe5a1bd710a6494eee3
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-08-22 06:52:19 +0600

    Delete operation added

--- index.js
+++ index.js
@@ -57,7 +57,14 @@ async function run() {
       res.send(result);
     });
 
-    
+    // delete operation for Mycraft data
+    app.delete("/all/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await All.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
